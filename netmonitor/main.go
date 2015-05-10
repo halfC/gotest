d8699e7d60dfb3699cd5cd44ee8e2d96c5a7d1de
zoo <zoophp@qq.com>
2015-05-10 22:38:32 +0800

    update 1

--- netmonitor/main.go
+++ netmonitor/main.go
@@ -1,24 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
 )
 
 func main() {
-	ln, err := net.Listen("tcp", ":8080")
+	ln, err := net.Listen("tcp", ":80")
 	if err != nil {
-        	log.Println(err)
-        	return
+		log.Println(err)
+		return
 	}
 	for {
-        	conn, err := ln.Accept()
-        	if err != nil {
-            	log.Println(err)
-            	continue
-        	}
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
 
-        	go echoFunc(conn)
+		go echoFunc(conn)
 	}
 }
 
@@ -26,12 +27,14 @@ func echoFunc(c net.Conn) {
 	buf := make([]byte, 1024)
 
 	for {
-        	n, err := c.Read(buf)
-        	if err != nil {
-            	log.Println(err)
-            	return
-        	}
+		n, err := c.Read(buf)
+		if err != nil {
+			log.Println(err)
+			return
+		}
 
-        	c.Write(buf[:n])
+		c.Write(buf[:n])
+		//log.Println(buf[:n])
+		fmt.Print(buf[:n])
 	}
 }
